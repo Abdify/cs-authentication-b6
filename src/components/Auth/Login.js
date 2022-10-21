2ff6de3b146eb383f0f5f8eb9c53ed9110d8f4c7
Abdify <abdullah.fahimm08@gmail.com>
2022-10-21 22:10:01 +0600

    context, private route, login in progress

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,17 +1,79 @@
-import React from 'react';
+import React, { useState } from "react";
 import { BiLogInCircle } from "react-icons/bi";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "../../styles/login.css";
 
 const Login = () => {
+  /**
+   * Get user info
+   * send these information to our login service
+   * get the user
+   * show the error
+   */
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+  };
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+      setErrors({...errors, email: "Please provide a valid email"})
+      setUserInfo({ ...userInfo, email: e.target.value });
+    } else {
+      setErrors({ ...errors, email: "" })
+      setUserInfo({ ...userInfo, email: e.target.value });
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+
+    if(password.length < 6){
+      setErrors({ ...errors, password: "Must be at least 6 characters" });
+      setUserInfo({ ...userInfo, password: e.target.value })
+    } else {
+      setErrors({...errors, password: "" })
+      setUserInfo({ ...userInfo, password: e.target.value })
+    }
+  }
+
   return (
     <div className="login-container">
-      <div className="login-title">Login<BiLogInCircle /></div>
-      <form className="login-form">
-        <input type="text" placeholder="Your Email" />
-        
-        <input type="password" placeholder="password" />
-        
+      <div className="login-title">
+        Login
+        <BiLogInCircle />
+      </div>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Your Email"
+          value={userInfo.email}
+          onChange={handleEmailChange}
+        />
+        {errors.email && <p className="error-message">{errors.email}</p> }
+
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={userInfo.password}
+          onChange={handlePasswordChange}
+        />
+        {errors.password && <p className="error-message">{errors.password}</p>}
         <button>Login</button>
 
         <p>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import Signup from "../components/Auth/Signup";
 import Generator from "../components/Generator/Generator";
 import Home from "../components/Home/Home";
 import Main from "../layout/Main";
+import PrivateRoute from "./PrivateRoute";
 
 const routes = createBrowserRouter([
   {
@@ -12,22 +13,26 @@ const routes = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "/generator",
-        element: <Generator />
+        element: (
+          <PrivateRoute>
+            <Generator />
+          </PrivateRoute>
+        ),
       },
       {
         path: "/login",
-        element: <Login />
+        element: <Login />,
       },
       {
         path: "/signup",
-        element: <Signup />
-      }
-    ]
-  }
-])
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 export default routes;
